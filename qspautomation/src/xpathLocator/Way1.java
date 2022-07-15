2a5989e9cc80853fba4e7c10c758fb49b5b6c0bc
QSP <QSP@DESKTOP-BH9RO97>
2022-07-15 15:18:07 +0530

    commit5

--- qspautomation/src/xpathLocator/Way1.java
+++ qspautomation/src/xpathLocator/Way1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginpageOrangeHrm {
+public class Way1 {
 
 	public static void main(String[] args) {
 		
