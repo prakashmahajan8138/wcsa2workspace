47d27b0daef3b86858b8be1eee0a8863f5ffa4c9
QSP <QSP@DESKTOP-BH9RO97>
2022-08-02 14:52:01 +0530

    neww1

--- qspautomation/src/actionsMethod/RightClick.java
+++ qspautomation/src/actionsMethod/RightClick.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DoubleClick {
+public class RightClick {
 
 	public static void main(String[] args) throws InterruptedException {
 
